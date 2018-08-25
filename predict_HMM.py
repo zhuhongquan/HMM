df18e90d6e8da5115e3c0a5e036c2f2ebad5a2a5
zhuhongquan <22229157@qq.com>
2018-08-25 18:40:12 +0800

    更新HMM代码文件

---
+++ predict_HMM.py
@@ -0,0 +1,100 @@
+import numpy as np
+import datetime
+from train_HMM import data_process
+
+# 读入分词和词性编号文件
+word_dict_file = open("word.dict", "r", encoding="utf-8")
+s1 = word_dict_file.read()
+word_dict = eval(s1)
+word_dict_file.close()
+tag_dict_file = open("tag.dict", "r", encoding="utf-8")
+s2 = tag_dict_file.read()
+tag_dict = eval(s2)
+tag_dict_file.close()
+# 读入状态转移矩阵和发射矩阵
+transition_matrix = np.loadtxt("transition_matrix.txt")
+emission_matrix = np.loadtxt("emission_matrix.txt")
+# 转换成对数
+emission_matrix = np.log(emission_matrix)
+transition_matrix = np.log(transition_matrix)
+
+
+def viterbi(word_list):
+    word_index = []
+    for word in word_list:
+        if word in word_dict.keys():
+            word_index.append(word_dict[word])
+        else:
+            word_index.append(word_dict['???'])
+
+    observe = len(word_list) + 1  # 观测序列，即一个句子的分词序列
+    states = len(tag_dict) - 2    # 隐藏状态，即分词可能的词性
+    max_prop = np.zeros((observe, states))
+    path = np.zeros((observe, states))
+
+    for i in range(states):
+        path[0][i] = -1
+        max_prop[0][i] = transition_matrix[-1][i] + emission_matrix[i][word_index[0]]
+
+    # 动态规划
+    for i in range(1, observe):
+        # 到达end状态有点区别
+        if i == observe - 1:
+            for k in range(states):
+                max_prop[i][k] = max_prop[i - 1][k] + transition_matrix[k][-1]
+                last_path = k
+                path[i][k] = last_path
+        else:
+            for j in range(states):
+                # 找出概率最大的路径(由于取了对数，所以采用加法)
+                prob = max_prop[i - 1] + transition_matrix[:-1, j] + emission_matrix[j][word_index[i]]
+                path[i][j] = np.argmax(prob)
+                max_prop[i][j] = max(prob)
+
+    gold_path = []
+    cur_state = observe - 1
+    step = np.argmax(max_prop[cur_state])
+    while True:
+        step = int(path[cur_state][step])
+        if step == -1:
+            break
+        gold_path.insert(0, step)
+        cur_state -= 1
+    return gold_path
+
+
+def evaluate(test_data):
+    total_words = 0
+    correct_words = 0
+    sentence_num = 0
+    print('正在标注词性...')
+    f = open('predict.txt', 'w', encoding='utf-8')  # 预测结果保存在predict.txt中
+    for sentence in test_data:
+        sentence_num += 1
+        word_list = []
+        tag_list = []
+        for word, tag in sentence:
+            word_list.append(word)
+            tag_list.append(tag)
+        predict = viterbi(word_list)  # 调用viterbi函数解码
+        total_words += len(sentence)
+        for i in range(len(predict)):
+            f.write(word_list[i] + '\t' + list(tag_dict.keys())[predict[i]] + '\n')
+            if predict[i] == tag_dict[tag_list[i]]:
+                correct_words += 1
+        f.write('\n')
+    f.close()
+    print('共{}个句子'.format(sentence_num))
+    print('共{}个单词，预测正确{}个分词'.format(total_words, correct_words))
+    print('准确率：{}'.format(correct_words / total_words))
+
+
+def main():
+    test_data = data_process('data/dev.conll')
+    start_time = datetime.datetime.now()
+    evaluate(test_data)
+    end_time = datetime.datetime.now()
+    print('共耗时' + str(end_time - start_time))
+
+
+main()
--- train_hmm.py
+++ train_hmm.py
@@ -1,140 +1,79 @@
+import numpy as np
 import re
-import collections
 
 
-# 对文件数据进行处理，提取出词和对应的词性放到列表里
-def data_processing(datafile):
-    with open(datafile, "r", encoding="utf-8") as datafile:
-        result_list = []
-        for row in datafile.readlines():
-            if row == "\n":
-                continue
-            else:
-                word = re.findall(r'^\d+\s(.+?)\s_\s', row)
-                tag = re.findall(r'_\s([A-Z]+?)\s_\s_\s\d+', row)
-                temp_list = []
-                temp_list.extend(word)
-                temp_list.extend(tag)
-                result_list.append(temp_list)
-    return result_list
-
-
-# 根据提取出的数据，建立状态转移矩阵
-def creat_transition_dict(data_list):
-    transition_dict = {}  # 用字典存放矩阵
-    for i in range(len(data_list)-1):
-        x = data_list[i][1]
-        y = data_list[i+1][1]
-        if x in transition_dict:
-            if y in transition_dict[x]:
-                transition_dict[x][y] += 1
-            else:
-                transition_dict[x][y] = 1
+# 对人工分词文件进行处理，提取出词和对应的词性[[(戴相龙,NR),(word,tag),(,)....],[],[]....]
+def data_process(data_file):
+    data_file = open(data_file, "r", encoding="utf-8")
+    data_list = []
+    sentence = []
+    for row in data_file.readlines():
+        if row == "\n":
+            data_list.append(sentence)
+            sentence = []
         else:
-            transition_dict[x] = {}
-            transition_dict[x][y] = 1
-    state_list = transition_dict.keys()  # 获取所有的状态
-    # 加1平滑并统计总数
-    count_transition = 0
-    for x in state_list:
-        for y in state_list:
-            if y not in transition_dict[x]:
-                transition_dict[x][y] = 1
-                count_transition += transition_dict[x][y]
+            word = re.findall(r'^\d+\s(.+?)\s_\s', row)
+            tag = re.findall(r'_\s([A-Z]+?)\s_\s_\s\d+', row)
+            sentence.append((word[0], tag[0]))
+    data_file.close()
+    return data_list
+
+
+def creat_matrix(train_data):  # train_data存放所有的训练句子，[[(戴相龙,NR),(,),(,)....],[],[]....]
+    tag_dict = {}  # tag_dict存放训练集中所有的tag，及其编号,考虑了起始和终止词性
+    word_dict = {}  # word_dict存放训练集中所有的word，及其编号,加入了未知词
+    for sentence in train_data:
+        for word, tag in sentence:
+            if word not in word_dict.keys():
+                word_dict[word] = len(word_dict)
+            if tag not in tag_dict.keys():
+                tag_dict[tag] = len(tag_dict)
+
+    tag_dict['BOS'] = len(tag_dict)
+    tag_dict['EOS'] = len(tag_dict)
+    word_dict['???'] = len(word_dict)
+    # 将分词和词性以及对应编号以字典形式保存到文件中
+    with open("word.dict", "w", encoding="utf-8") as word_dict_file:
+        word_dict_file.write(str(word_dict))
+    with open("tag.dict", "w", encoding="utf-8") as tag_dict_file:
+        tag_dict_file.write(str(tag_dict))
+    # 第(i,j)个元素表示词性j在词性i后面的概率（拓展了1行1列，最后一行是start，最后一列是stop）
+    transition_matrix = np.zeros([len(tag_dict) - 1, len(tag_dict) - 1])
+    # 第(i,j)个元素表示词性i发射到词j的概率
+    emission_matrix = np.zeros([len(tag_dict) - 2, len(word_dict)])
+
+    # 计算发射矩阵参数
+    alpha = 0.1
+    for sentence in train_data:
+        for word, tag in sentence:
+            emission_matrix[tag_dict[tag]][word_dict[word]] += 1
+    for i in range(len(emission_matrix)):
+        s = sum(emission_matrix[i])
+        for j in range(len(emission_matrix[i])):
+            emission_matrix[i][j] = (emission_matrix[i][j] + alpha) / (s + alpha * (len(word_dict)))  # 加alpha平滑
+
+    # 计算转移矩阵参数
+    for i in range(len(train_data)):
+        for j in range(len(train_data[i]) + 1):
+            if j == 0:
+                transition_matrix[-1][tag_dict[train_data[i][j][1]]] += 1  # 初始tag频率
+            elif j == len(train_data[i]):
+                transition_matrix[tag_dict[train_data[i][j - 1][1]]][-1] += 1  # 结束tag频率
             else:
-                transition_dict[x][y] += 1
-                count_transition += transition_dict[x][y]
-    # 计算概率
-    for x in state_list:
-        for y in state_list:
-            transition_dict[x][y] = transition_dict[x][y]/count_transition
-    # 将转移矩阵以字典的形式保存到文件中
-    with open("transition.dict", "w", encoding="utf-8") as transition_dict_file:
-        transition_dict_file.write(str(transition_dict))
+                transition_matrix[tag_dict[train_data[i][j - 1][1]]][tag_dict[train_data[i][j][1]]] += 1
 
-    '''
-    # 打印状态转移矩阵
-    print("      ", end='')
-    for x in state_list:
-        print("{:<9}".format(x), end='')
-    print()
-    for x in state_list:
-        print("{:<6}".format(x), end='')
-        for y in state_list:
-            print("{:.6f}".format(transition_dict[x][y]) + ' ', end='')
-        print()
-    '''
-
-
-# 计算发射(混淆)矩阵
-def creat_emission_dict(data_list):
-    emission_dict = {}
-    data_tuple = []
-    for i in data_list:
-        data_tuple.append(tuple(i))  # 将列表转为元祖，为counter函数做准备
-    dict1 = collections.Counter(data_tuple)  # 该字典记录的是混淆矩阵中对应着同一个分词的隐藏状态的数量
-    # 统计每一个词性出现的频率(即隐藏状态出现的频率)
-    state_list = []  # 存放词性(隐藏状态) 作为矩阵的行
-    obs_list = []    # 存放分词(观测状态) 作为矩阵的列
-    for i in data_list:  # 去除重复值
-        if i[0] not in obs_list:
-            obs_list.append(i[0])
-        if i[1] not in state_list:
-            state_list.append(i[1])
-
-    for state in state_list:
-        emission_dict[state] = {}  # 初始化混淆矩阵的隐藏状态
-    # 统计数目并加1平滑
-    for state in state_list:
-        for obs in obs_list:  # 遍历所有观测状态
-            temp_tuple = (obs, state)
-            if temp_tuple in dict1:  # 查找(词,词性)出现的频率
-                emission_dict[state][obs] = dict1[temp_tuple] + 1
-            else:
-                emission_dict[state][obs] = 1
-    # 统计分母
-    state_dict = {}
-    for state in emission_dict:
-        count_state = 0
-        for obs in emission_dict[state]:
-            count_state += emission_dict[state][obs]
-        state_dict[state] = count_state
-    # 计算概率
-    for state in emission_dict:
-        for obs in emission_dict[state]:
-            emission_dict[state][obs] = emission_dict[state][obs]/state_dict[state]
-    with open("emission.dict", "w", encoding="utf-8") as emission_dict_file:
-        emission_dict_file.write(str(emission_dict))
-
-
-# 计算初始概率
-def get_start_probability(datafile):
-    start_state = []
-    datafile =  open(datafile, "r", encoding="utf-8")
-    line = datafile.readline()
-    while line != '':
-        tag = re.findall(r'_\s([A-Z]+?)\s_\s_\s\d+', line)
-        start_state.append(tag[0])
-        while datafile.readline() != '\n':
-            continue
-        line = datafile.readline()
-    start_prob = {}
-    for tag in start_state:
-        if tag not in start_prob:
-            start_prob[tag] = 1
-        else:
-            start_prob[tag] += 1
-    for tag in start_prob:
-        start_prob[tag] = start_prob[tag] / len(start_state)
-    with open("start_prob.dict", "w", encoding="utf-8") as start_prob_file:
-        start_prob_file.write(str(dict(start_prob)))
+    for i in range(len(transition_matrix)):
+        s = sum(transition_matrix[i])
+        for j in range(len(transition_matrix[i])):
+            transition_matrix[i][j] = (transition_matrix[i][j] + alpha) / (s + alpha * (len(tag_dict) - 1))
+    return transition_matrix, emission_matrix
 
 
 def main():
-    data_list = data_processing("train.conll")
-    creat_transition_dict(data_list)
-    creat_emission_dict(data_list)
-    get_start_probability("train.conll")
+    train_data = data_process('data/train.conll')
+    transition_matrix, emission_matrix = creat_matrix(train_data)
+    np.savetxt("transition_matrix.txt", transition_matrix)
+    np.savetxt("emission_matrix.txt", emission_matrix)
 
 
 main()
